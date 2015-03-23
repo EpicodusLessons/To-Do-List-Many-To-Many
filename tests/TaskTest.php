db367de33f7c29de71156dac4477f95ba62d7faf
Diane Douglas <diane.douglas1@gmail.com>
2015-03-23 00:54:33 -0700

    Add failing tests for save, getAll and deleteAll for Task class with skeleton methods.

--- tests/TaskTest.php
+++ tests/TaskTest.php
@@ -82,7 +82,100 @@
             $this->assertEquals(2, $result);
         }
 
+        //CREATE - save method stores all object data in tasks table.
+        function testSave()
+        {
+            //Arrange
+            //create a new task
+            $description = "Wash the dog";
+            $id = 1;
+            $test_task = new Task($description, $id);
 
+            //Act
+            //save the task to the database
+            //Id should be assigned in database, then stored in object.
+            $test_task->save();
+
+            //Assert
+            //get all existing tasks back out of the database. 
+            //The first and only one should hold the same properties as the test task.
+            $result = Task::getAll();
+            $this->assertEquals($test_task, $result[0]);
+        }
+
+        //This test makes sure that after saving not only are the id's equal, they are not null.
+        function testSaveSetsId()
+        {
+            //Arrange
+            //create new task
+            $description = "Wash the dog";
+            $id = 1;
+            $test_task = new Task($description, $id);
+
+            //Act
+            //save it. Id should be assigned in database, then stored in object.
+            $test_task->save();
+
+            //Assert
+            //That id in the object should be numeric (not null)
+            $this->assertEquals(true, is_numeric($test_task->getId()));
+        }
+
+        //READ - All tasks
+        //Can't run the previous two tests without getAll().
+        //This method should return an array of all Task objects from the tasks table.
+        //Since it isn't specifically for only one Task, it is for all, it should be a static method.
+        function testGetAll()
+        {
+            //Arrange
+            //Create and save more than one Task object.
+            $description = "Wash the dog";
+            $id = 1;
+            $test_task = new Task($description, $id);
+            $test_task->save();
+
+
+            $description2 = "Water the lawn";
+            $id2 = 2;
+            $test_task2 = new Task($description2, $id2);
+            $test_task2->save();
+
+            //Act
+            //Query the database to get all existing saved tasks as objects.
+            $result = Task::getAll();
+
+            //Assert
+            //We should get our two test tasks back out in $result. 
+            //Remember the [$thing1, $thing2] notation is used for an array.
+            $this->assertEquals([$test_task, $test_task2], $result);
+        }
+
+        //Now that we are saving, we need a method to delete everything out of our database too.
+        //For our tests to run we need to clear our test database with a deleteAll function after each test.
+        //Since this also deals with more than one Task it should be a static method.
+        function testDeleteAll()
+        {
+            //Arrange
+            //We need some tasks saved into the database so that we can make sure our deleteAll method removes them all.
+            $description = "Wash the dog";
+            $id = 1;
+            $test_task = new Task($description, $id);
+            $test_task->save();
+
+            $description2 = "Water the lawn";
+            $id2 = 2;
+            $test_task2 = new Task($description2, $id2);
+            $test_task2->save();
+
+            //Act
+            //Set all tasks on fire. Delete them.
+            Task::deleteAll();
+
+            //Assert
+            //Now when we call getAll, we should get an empty array because we deleted all tasks.
+            $result = Task::getAll();
+            $this->assertEquals([], $result);
+        }
         
     }
 ?>
