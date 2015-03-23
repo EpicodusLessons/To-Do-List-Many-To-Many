2ee0ddec1ac687044fb991a8e8e98ce933bd3278
Diane Douglas <diane.douglas1@gmail.com>
2015-03-23 01:29:51 -0700

    Pass tests by adding save, getAll and deleteAll methods to Category class.

--- src/Category.php
+++ src/Category.php
@@ -33,17 +33,27 @@
 
         function save()
         {
-            return null;
+            $statement = $GLOBALS['DB']->query("INSERT INTO categories (name) VALUES ('{$this->getName()}') RETURNING id;");
+            $result = $statement->fetch(PDO::FETCH_ASSOC);
+            $this->setId($result['id']);
         }
 
         static function getAll()
         {
-            return null;
+            $returned_categories = $GLOBALS['DB']->query("SELECT * FROM categories;");
+            $categories = array();
+            foreach($returned_categories as $category) {
+                $name = $category['name'];
+                $id = $category['id'];
+                $new_category = new Category($name, $id);
+                array_push($categories, $new_category);
+            }
+            return $categories;
         }
 
         static function deleteAll()
         {
-            return null;
+            $GLOBALS['DB']->exec("DELETE FROM categories *;");
         }
     }
 
