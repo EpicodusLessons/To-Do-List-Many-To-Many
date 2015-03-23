2455b10074ed5f16f4c474ab2b50f8da28a41a4b
Diane Douglas <diane.douglas1@gmail.com>
2015-03-23 00:07:49 -0700

    Add app.php and index.php with quick test route that prints hi! at the root path to make sure all files are loaded correctly. Include Task and Category class files in app.php with Twig path, and HttpFoundation\Request lines.

---
+++ app/app.php
@@ -0,0 +1,25 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Task.php";
+    require_once __DIR__."/../src/Category.php";
+
+    $app = new Silex\Application();
+    $app['debug'] = true;
+
+    $DB = new PDO('pgsql:host=localhost;dbname=to_do');
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    $app->get("/", function() use ($app) {
+        return "Hi!";
+    });
+
+
+    return $app;
+
+?>
