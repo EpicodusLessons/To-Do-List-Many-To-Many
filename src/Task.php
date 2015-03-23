7279c760fb682cc6f50ee1143d0e4e91427afed7
Diane Douglas <diane.douglas1@gmail.com>
2015-03-23 00:21:25 -0700

    Add first 2 Task tests for basic property getters and setters and make them pass. Give Task class a private property to hold the string description as well as constructor, getter and setter for the task's description.

--- src/Task.php
+++ src/Task.php
@@ -2,6 +2,22 @@
 
     class Task
     {
+        private $description;
+
+        function __construct($initial_description)
+        {
+            $this->description = $initial_description;
+        }
+
+        function getDescription()
+        {
+            return $this->description;
+        }
+
+        function setDescription($new_description)
+        {
+            $this->description = (string) $new_description;
+        }
         
     }
 
--- tests/TaskTest.php
+++ tests/TaskTest.php
@@ -18,6 +18,36 @@
 
         }
 
+        //Initialize a Task with a description and be able to get it back out of the object using getDescription().
+        function test_getDescription()
+        { 
+            //Arrange
+            $description = "Do dishes.";
+            $test_task = new Task($description);
+
+            //Act
+            $result = $test_task->getDescription();
+
+            //Assert
+            $this->assertEquals($description, $result);
+
+        }
+
+        function test_setDescription()
+        { //can I change the description in the object with setDescription() after initializing it?
+            //Arrange
+            $description = "Do dishes.";
+            $test_task = new Task($description);
+
+            //Act
+            $test_task->setDescription("Drink coffee.");
+            $result = $test_task->getDescription();
+
+            //Assert
+            $this->assertEquals("Drink coffee.", $result);
+
+        }
+
         
     }
 ?>
