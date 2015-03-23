e5949af117cace0efefcafdee90056c64321cb9f
Diane Douglas <diane.douglas1@gmail.com>
2015-03-23 08:13:54 -0700

    Add forms and routes to be able to assign tasks to a category and a category to a task inside of the task.html.twig and category.html.twig views.

--- app/app.php
+++ app/app.php
@@ -33,13 +33,13 @@
     //READ (singular) task
     $app->get("/tasks/{id}", function($id) use ($app) {
         $task = Task::find($id);
-        return $app['twig']->render('task.html.twig', array('task' => $task));
+        return $app['twig']->render('task.html.twig', array('task' => $task, 'categories' => $task->getCategories(), 'all_categories' => Category::getAll()));
     });
 
     //READ (singular) category
     $app->get("/categories/{id}", function($id) use ($app) {
         $category = Category::find($id);
-        return $app['twig']->render('category.html.twig', array('category' => $category));
+        return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => $category->getTasks(), 'all_tasks' => Task::getAll()));
     });
 
     //post
@@ -58,6 +58,22 @@
         $category = new Category($name);
         $category->save();
         return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+    });
+
+    //CREATE add tasks to category. send here from form in category.html.twig
+    $app->post("/add_tasks", function() use ($app) {
+        $category = Category::find($_POST['category_id']);
+        $task = Task::find($_POST['task_id']);
+        $category->addTask($task);
+        return $app['twig']->render('category.html.twig', array('category' => $category, 'categories' => Category::getAll(), 'tasks' => $category->getTasks(), 'all_tasks' => Task::getAll()));
+    });
+
+    //CREATE add categories to task. send here from form in task.html.twig
+    $app->post("/add_categories", function() use ($app) {
+        $category = Category::find($_POST['category_id']);
+        $task = Task::find($_POST['task_id']);
+        $task->addCategory($category);
+        return $app['twig']->render('task.html.twig', array('task' => $task, 'tasks' => Task::getAll(), 'categories' => $task->getCategories(), 'all_categories' => Category::getAll()));
     });
 
     //delete
